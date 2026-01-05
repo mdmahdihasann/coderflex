1d01cb95935a4593646d3ebf224af33900490107
mdmahdihasann <mdmahdicmt123@gmail.com>
2026-01-05 23:19:42 +0600

    updated simple

--- components/details/Video.jsx
+++ components/details/Video.jsx
@@ -1,19 +1,18 @@
-
+"use client";
 const Video = ({ video }) => {
   if (!video?.videoURL) {
     return <p className="text-red-500">Video URL not found</p>;
   }
-  console.log(video.videoURL);
 
   return (
     <div className="relative aspect-video rounded-xl overflow-hidden bg-black ring-1 ring-white/10">
       <iframe
-        className="w-full h-full"
+        class="w-full h-full"
         src={video.videoURL}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
